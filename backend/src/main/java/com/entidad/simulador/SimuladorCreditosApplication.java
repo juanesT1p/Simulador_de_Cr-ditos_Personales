0263407bb07d1307622a864ea07ec5aa59a11355
juanesT1p <jept2406@gmail.com>
2026-08-22 17:11:10 -0500

    chore: crea estructura inicial del simulador de créditos

---
+++ backend/src/main/java/com/entidad/simulador/SimuladorCreditosApplication.java
@@ -0,0 +1,12 @@
+package com.entidad.simulador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorCreditosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimuladorCreditosApplication.class, args);
+    }
+}
